2fd009f64f0c0a70b8366e4647d6f9c24cfa9fd7
Kevin Shi <xpspectre@gmail.com>
2014-01-25 16:55:59 -0500

    Added support for reverse rxns, some syntax changes to input reaction format 'raw' vs massaction kinetics

--- parse_input.m
+++ parse_input.m
@@ -16,37 +16,65 @@ function [settings,constants,species,reactions] =  parse_input(filename)
     reactions_strs = data.reactions;
     r_names = fieldnames(reactions_strs);
     for i = 1:length(r_names)
+        
+        reactants = [];
+        products = [];
         rxn = reactions_strs.(r_names{i});
-        
+                
         % Get [reactants_str] ->{[rate_str]} [products_str]
-        [~,tok_i,~] = regexp(rxn,'(.*?)->\{(.*)}(.*)','match','tokens');
-        reactants_str = tok_i{1,1}{1,1};
-        rate_str = tok_i{1,1}{1,2};
-        products_str = tok_i{1,1}{1,3};
-        
-        clear reactants products
+        p1 = '^(?<reactants>.*?)';
+        p2 = '(?<f_rate>->\{[^}]*\})';
+        p3 = '(?<r_rate><-\{[^}]*\})?';
+        p4 = '(?<products>.*?)$';
+        expr = [p1 p2 p3 p4];
+        parts = regexp(rxn,expr,'names');
         
         % Get reactant species and stoichimetries
-        [~,tok_j,~] = regexp(reactants_str,'([0-9]*)([A-Za-z0-9_])','match','tokens');
-        for j = 1:length(tok_j)
-            if strcmpi(tok_j{1,j}{1,1},'') % empty coefficient => 1
-                tok_j{1,j}{1,1} = '1';
+        tok = regexp(parts.reactants,'([0-9]*)([A-Za-z0-9_])*','tokens');
+        for j = 1:length(tok)
+            if strcmpi(tok{1,j}{1,1},'') % empty coefficient => stoich 1
+                tok{1,j}{1,1} = '1';
             end
-            reactants.(tok_j{1,j}{1,2}) = str2double(tok_j{1,j}{1,1});
+            reactants.(tok{1,j}{1,2}) = str2double(tok{1,j}{1,1});
         end
         
         % Get product species and stoichimetries
-        [~,tok_j,~] = regexp(products_str,'([0-9]*)([A-Za-z0-9_])','match','tokens');
-        for j = 1:length(tok_j)
-            if strcmpi(tok_j{1,j}{1,1},'') % empty coefficient => 1
-                tok_j{1,j}{1,1} = '1';
+        tok = regexp(parts.products,'([0-9]*)([A-Za-z0-9_])*','tokens');
+        for j = 1:length(tok)
+            if strcmpi(tok{1,j}{1,1},'') % empty coefficient => stoich 1
+                tok{1,j}{1,1} = '1';
             end
-            products.(tok_j{1,j}{1,2}) = str2double(tok_j{1,j}{1,1});
+            products.(tok{1,j}{1,2}) = str2double(tok{1,j}{1,1});
+        end
+        
+        % Process forward rxn ( of the form: ->{[rate]} )
+        %   Assumes a forward rate is always present
+        f_rate_str = parts.f_rate(4:end-1);
+        if strcmpi(f_rate_str(1),'''') == 1 && strcmpi(f_rate_str(end),'''') == 1
+            f_rate_type = 'raw';
+        else % default
+            f_rate_type = 'massaction';
+        end
+        
+        % Process reverse rate ( of the form: <-{[rate]} ) if present
+        %   Assumes a forward rate is always present
+        if strcmpi(parts.r_rate,'') || isempty(parts.r_rate(4:end-1)) % forward rxn only
+            r_rate_str = '';
+            r_rate_type = 'none';
+        else % reversible rxn
+            r_rate_str = parts.r_rate(4:end-1);
+            if strcmpi(r_rate_str(1),'''') == 1 && strcmpi(r_rate_str(end),'''') == 1
+                r_rate_type = 'raw';
+            else % default
+                r_rate_type = 'massaction';
+            end
         end
         
         % Build reactions struct
         reactions.(r_names{i}).reactants = reactants;
         reactions.(r_names{i}).products = products;
-        reactions.(r_names{i}).rate_str = rate_str;
-        
+        reactions.(r_names{i}).f_rate_str = f_rate_str;
+        reactions.(r_names{i}).f_rate_type = f_rate_type;
+        reactions.(r_names{i}).r_rate_str = r_rate_str;
+        reactions.(r_names{i}).r_rate_type = r_rate_type;
     end
